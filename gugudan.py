7bae2ba615d7aaa8fd56b97f713bd49359166a02
yunjuna <yunjuna@gmail.com>
2024-06-10 11:14:35 +0900

    Add gugudan.py with gugudan program

---
+++ gugudan.py
@@ -0,0 +1,9 @@
+# gugudan.py
+def gugudan():
+    for i in range(1, 10):
+        for j in range(1, 10):
+            print(f"{i} x {j} = {i * j}")
+        print()
+
+if __name__ == "__main__":
+    gugudan()
